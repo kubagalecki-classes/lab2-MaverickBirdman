5bd58eeb35eb79b78121f13fc3bfb69326a43462
MaverickBirdman <73669666+MaverickBirdman@users.noreply.github.com>
2020-11-11 23:08:17 +0100

    Update ResourceManager.hpp
    partially done

--- zal/ResourceManager.hpp
+++ zal/ResourceManager.hpp
@@ -1,8 +1,18 @@
 #pragma once
-
 #include "Resource.hpp"
-
 class ResourceManager
-{
-    // Twoja implementacja tutaj
+{Resource* zmienna;
+public:
+ResourceManager()//dom
+        {zmienna=new Resource;}
+ResourceManager(const ResourceManager &obiekt)//kop
+        {zmienna = new Resource{ *obiekt.zmienna };}
+ResourceManager operator=(const ResourceManager& obiekt)//kop op przyp
+    {if (&obiekt != this) 
+        {delete zmienna;
+         zmienna = new Resource{ *obiekt.zmienna };}
+         return *this;}
+ ~ResourceManager()//des
+        {delete zmienna;}
+ double get(){return zmienna->get();}//fun
 };
