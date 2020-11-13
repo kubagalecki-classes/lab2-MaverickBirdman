63f95fd1aada6e02c35bdf435a180dde560a287e
MaverickBirdman <73669666+MaverickBirdman@users.noreply.github.com>
2020-11-13 14:59:28 +0100

    Update ResourceManager.hpp

--- zal/ResourceManager.hpp
+++ zal/ResourceManager.hpp
@@ -1,18 +1,37 @@
 #pragma once
 #include "Resource.hpp"
-class ResourceManager
-{Resource* zmienna;
+
+class ResourceManager{
+ Resource* zmienna;
+
 public:
-ResourceManager()//dom
-        {zmienna=new Resource;}
-ResourceManager(const ResourceManager &obiekt)//kop
-        {zmienna = new Resource{ *obiekt.zmienna };}
-ResourceManager operator=(const ResourceManager& obiekt)//kop op przyp
+ 
+ResourceManager()// konstruktor domyślny
+ {zmienna=new Resource;}
+   
+double get(){return zmienna->get();}//metoda
+    
+ResourceManager(const ResourceManager &obiekt)//konstruktor kopiujący
+    {zmienna = new Resource{ *obiekt.zmienna };}
+   
+ResourceManager operator=(const ResourceManager& obiekt)//kopiujący operator przypisania
     {if (&obiekt != this) 
         {delete zmienna;
-         zmienna = new Resource{ *obiekt.zmienna };}
-         return *this;}
- ~ResourceManager()//des
-        {delete zmienna;}
- double get(){return zmienna->get();}//fun
+            zmienna = new Resource{ *obiekt.zmienna };}
+        return *this;}
+    
+ResourceManager(ResourceManager&& obiekt)
+    {zmienna = nullptr;
+        zmienna = obiekt.zmienna;
+        obiekt.zmienna = nullptr;}
+
+ResourceManager operator=(ResourceManager&& obiekt)
+    {if (&obiekt != this) 
+        {delete zmienna;
+            zmienna = obiekt.zmienna;
+            obiekt.zmienna = nullptr;}
+        return *this;}
+ 
+ ~ResourceManager()//destruktor
+    {delete zmienna;}
 };
